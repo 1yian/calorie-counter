f7961fddff0d42f05524565e7f2b1ad233bf0093
1yian <yian@utexas.edu>
2024-01-06 19:15:08 -0600

    Changed the title :D

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Streamlit app
 def main():
-    st.title("Image and Notes Submission to GPT-4 Vision")
+    st.title("Yian and Milton's Calorie Counter :D")
 
     api_key = os.environ.get('OPENAI_API_KEY')
     vision_api = OpenAIVisionAPI(api_key)
@@ -14,7 +14,7 @@ def main():
     text_query = st.text_area("Enter your notes")
 
     if st.button("Submit"):
-        if image_file is not None and text_query:
+        if image_file is not None:
             image = Image.open(image_file)
             try:
                 cals, fats, proteins, carbs = vision_api.query_vision_model(image, text_query)
--- backend.py
+++ backend.py
@@ -4,6 +4,12 @@ from PIL import Image
 import io
 import re
 
+DEFAULT_PROMPT = "To the best of your ability, come up with a maximum " + \
+"likelihood estimate of the macro and calorie count of this meal based on how " + \
+"it looks alone. When you come up with your estimate, call the estimate " + \
+"function by saying 'estimate(x, p, f, c)' where x is your best guess of " + \
+"the calories, p is protein, f is fats, and c is carbs."
+
 class OpenAIVisionAPI:
     def __init__(self, api_key):
         self.api_key = api_key
@@ -31,7 +37,15 @@ class OpenAIVisionAPI:
 
         return function_calls
 
-    def query_vision_model(self, image, text_query):
+    def create_prompt(self, notes: str):
+        if notes:
+            return DEFAULT_PROMPT + f" Here are some notes about the meal: {notes}"
+        else:
+            return DEFAULT_PROMPT
+
+
+    def query_vision_model(self, image, notes):
+        text_input = self.create_prompt(notes)
         base64_image = self.encode_image(image)
         response = openai.ChatCompletion.create(
             model="gpt-4-vision-preview",
@@ -39,7 +53,7 @@ class OpenAIVisionAPI:
                 {
                     "role": "user",
                     "content": [
-                        {"type": "text", "text": text_query},
+                        {"type": "text", "text": text_input},
                         {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
                     ]
                 }
@@ -49,6 +63,9 @@ class OpenAIVisionAPI:
         )
         answer = response.choices[0].message['content']
 
-        calories, fats, proteins, carbs = self.find_function_call(answer)[0]
-
+        macros_found = self.find_function_call(answer)
+        if len(macros_found) > 0:
+            calories, fats, proteins, carbs = macros_found[0]
+        else:
+            assert False, f"Could not find anything {answer}"
         return calories, fats, proteins, carbs
